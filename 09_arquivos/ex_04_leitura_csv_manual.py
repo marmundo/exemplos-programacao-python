880f3542b0f68bb5a4a17c0dce6a1bd4e9fc6284
Felipe A P Pinto <felipe.pinto@ifrn.edu.br>
2023-07-21 10:04:03 -0300

    .

--- 09_arquivos/ex_04_leitura_csv_manual.py
+++ 09_arquivos/ex_04_leitura_csv_manual.py
@@ -2,6 +2,7 @@ arquivo = open("contatos.csv", "r", encoding="utf-8")
 
 for linha in arquivo:
     nome, email, telefone = linha.strip().split(";")
+
     print(nome)
     print(email)
     print(telefone)
